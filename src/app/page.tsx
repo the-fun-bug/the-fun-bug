57118ba07672803e9af0094a8a347f6d91315078
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-09-09 09:56:15 +0200

    remove coming soon page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
 import GlobalLayout from '@/components/common/GlobalLayout';
-import Image from 'next/image';
-import funBugLogo from './The Fun Bug Logo - LARGE.png';
+import Homepage from '@/components/home/Homepage';
 
 export default function Home() {
   return (
     <GlobalLayout>
-      <main className="bg-white gap-[2rem] flex flex-col justify-center items-center h-screen p-[1rem]">
-        <h1 className="font-nickainley text-center">Coming Soon...</h1>
-        <Image
-          src={funBugLogo}
-          alt="The Fun Bug, Play Studio and Parties"
-          className="pointer-events-none w-full max-w-[500px]"
-          height={682}
-          width={970}
-        />
-      </main>
+      <Homepage />
     </GlobalLayout>
   );
 }
