b4870439a6f98b94c49d9b973e6b203c7e73d19d
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-09-10 11:18:56 +0200

    add contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import GlobalLayout from '@/components/common/GlobalLayout';
+import VisitUs from '@/components/common/VisitUs';
+
+export default function ContactPage() {
+  return (
+    <GlobalLayout>
+      <VisitUs />
+    </GlobalLayout>
+  );
+}
