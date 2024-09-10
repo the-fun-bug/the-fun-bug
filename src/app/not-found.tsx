ec9c91164c97edd40596ab7293dd5fdda1addddc
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-09-10 11:34:44 +0200

    add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import GlobalLayout from '@/components/common/GlobalLayout';
+import ButtonLink from '@/components/common/ButtonLink';
+
+export default function NotFound() {
+  return (
+    <GlobalLayout>
+      <div className="flex flex-col items-center justify-center gap-[2rem] h-[calc(100vh-252px)]">
+        <h1 className="font-nickainley text-center max-w-[390px]">
+          Oops! There’s Not a Page Here.
+        </h1>
+        <ButtonLink
+          buttonText="Back To Homepage"
+          buttonLink="/"
+          buttonClass="bg-soft-pink hover:bg-dark-pink hover:text-white"
+        />
+      </div>
+    </GlobalLayout>
+  );
+}
