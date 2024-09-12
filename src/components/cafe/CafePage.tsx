410ff32a9182530b9899b0c9a3d5242b7c778947
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-09-12 17:56:28 +0200

    add cafe page header and menu and minor changes to nav

---
+++ src/components/cafe/CafePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CafeHeader from './CafeHeader';
+import CafeMenu from './CafeMenu';
+
+export default function CafePage() {
+  return (
+    <>
+      <CafeHeader />
+      <CafeMenu />
+    </>
+  );
+}
