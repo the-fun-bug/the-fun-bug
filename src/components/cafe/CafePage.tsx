14e654a210a328f4b59de23d613f9a859fe8b6da
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-09-12 18:15:36 +0200

    add last section to cafe page

--- src/components/cafe/CafePage.tsx
+++ src/components/cafe/CafePage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import CafeHeader from './CafeHeader';
 import CafeMenu from './CafeMenu';
+import DidYouKnow from './DidYouKnow';
 
 export default function CafePage() {
   return (
     <>
       <CafeHeader />
       <CafeMenu />
+      <DidYouKnow />
     </>
   );
 }
