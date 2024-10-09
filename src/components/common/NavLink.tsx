d4ab1d2539b68f6ff9341a913424834751e9efd6
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-10-09 16:25:44 +0200

    style updates from review

--- src/components/common/NavLink.tsx
+++ src/components/common/NavLink.tsx
@@ -5,17 +5,19 @@ type NavLinkProps = {
   linkName: string;
   link: string;
   isActive?: boolean; // Added isActive prop
+  footer?: boolean;
 };
 
 export default function NavLink({
   linkName,
   link,
   isActive = false,
+  footer = false,
 }: NavLinkProps) {
   return (
     <Link
       href={link}
-      className={`font-bold border-b-[1px] border-worm-blue md:border-none md:rounded-lg px-[1.25rem] py-[0.75rem] transition-all duration-300 ease-in-out 
+      className={`w-fit font-bold ${footer ? '' : 'border-b-[1px] border-worm-blue md:border-none'} md:rounded-lg px-[1.25rem] py-[0.75rem] transition-all duration-300 ease-in-out 
       ${isActive ? 'bg-soft-blue' : ''} 
       md:hover:bg-soft-blue`} // Conditionally apply active styles
     >
