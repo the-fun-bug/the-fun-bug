b1dc194a8474284859745f853a7396e1e00e6e84
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-10-16 18:27:01 +0200

    fix play houses

--- src/components/home/HowWePlay.tsx
+++ src/components/home/HowWePlay.tsx
@@ -1,57 +1,78 @@
 import React from 'react';
 import Image from 'next/image';
-import openPlayBg from './img/open-play.png';
-import partiesBg from './img/parties.png';
 import ButtonLink from '../common/ButtonLink';
+import pinkTopImg from './img/pink-top.png';
+import blueTopImg from './img/blue-top.png';
 
 export default function HowWePlay() {
   return (
     <section className="py-[3rem] flex flex-col justify-center items-center px-[1rem]">
       <div className="w-full max-w-[1200px] text-center flex flex-col gap-[1rem] justify-center items-center">
-        <h1 className="font-nickainley pb-[2rem]">This Is How We Play</h1>
+        <h1 className="font-nickainley pb-[100px]">This Is How We Play</h1>
         <div className="flex flex-col justify-between items-center lg:flex-row w-full gap-[3rem]">
-          <div className="relative w-fit">
-            <Image
-              src={openPlayBg}
-              alt="blue play house"
-              width={920}
-              height={854}
-              className="max-w-[460px] w-full"
-            />
-            <div className="absolute top-0 left-0 pt-[90px] xxs:pt-[120px] px-[2.25rem] xs:px-[3rem] flex flex-col justify-center items-center gap-[1rem] xs:gap-[1.75rem] max-w-[460px] w-full">
-              <h2 className="xs:mt-[1rem]">Play Studio</h2>
-              <p className="">
-                Kids will have a blast on our large custom-built wooden play
-                structure, climb on our soft play mats, and explore their
-                creativity with endless play opportunities *
-              </p>
-              <ButtonLink
-                buttonText="Pricing & Memberships"
-                buttonLink="/pricing"
-                buttonClass="bg-soft-blue hover:bg-worm-blue my-0"
+          <div className="flex justify-center lg:w-[50%]">
+            <div className="relative w-full max-w-[460px] flex-items-center">
+              <Image
+                src={blueTopImg}
+                width={920}
+                height={206}
+                className="absolute top-[-15%] max-w-[460px] w-full"
+                alt="scalloped play house top"
               />
+              <div className="flex justify-center">
+                <div className="bg-soft-blue pt-[3rem] pb-[1.5rem] sm:pb-[2.75rem] px-[1rem] sm:px-[1.75rem] flex flex-col justify-center items-center gap-[1.5rem] rounded-b-xl max-w-[90%]">
+                  <h2 className="xs:mt-[1rem]">Play Studio</h2>
+                  <p className="max-w-[350px]">
+                    Kids will have a blast on our large custom-built wooden play
+                    structure, climb on our soft play mats, and explore their
+                    creativity with endless play opportunities *
+                  </p>
+                  <ButtonLink
+                    buttonText="Pricing & Memberships"
+                    buttonLink="/pricing"
+                    buttonClass="hidden sm:block bg-soft-blue hover:bg-worm-blue"
+                  />
+                  <ButtonLink
+                    buttonText="Pricing"
+                    buttonLink="/pricing"
+                    buttonClass="block sm:hidden bg-soft-blue hover:bg-worm-blue"
+                  />
+                </div>
+              </div>
             </div>
           </div>
-          <div className="relative w-fit">
-            <Image
-              src={partiesBg}
-              alt="pink play house"
-              width={920}
-              height={854}
-              className="max-w-[460px] w-full"
-            />
-            <div className="absolute top-0 left-0 pt-[90px] xxs:pt-[120px] px-[2.25rem] xs:px-[3rem] flex flex-col justify-center items-center gap-[1rem] xs:gap-[1.75rem] max-w-[460px] w-full">
-              <h2 className="xs:mt-[1rem]">Parties & Events</h2>
-              <p className="">
-                Book your party with us and enjoy full, private access to our
-                facility. The venue will be exclusively yours, closed to the
-                public. Regular snack & drink menu will be available *
-              </p>
-              <ButtonLink
-                buttonText="Start Planning Your Event"
-                buttonLink="/parties"
-                buttonClass="bg-soft-pink hover:bg-worm-pink my-0"
-              />
+          <div className="flex justify-center lg:w-[50%] pt-[3rem] lg:pt-0">
+            <div className="flex flex-col justify-between items-center lg:flex-row w-full gap-[3rem]">
+              <div className="relative w-full max-w-[460px] flex-items-center">
+                <Image
+                  src={pinkTopImg}
+                  width={920}
+                  height={206}
+                  className="absolute top-[-15%] max-w-[460px] w-full"
+                  alt="scalloped play house top"
+                />
+                <div className="flex justify-center">
+                  <div className="bg-soft-pink pt-[3rem] pb-[1.5rem] sm:py-[2.75rem] px-[1rem] sm:px-[1.75rem] flex flex-col justify-center items-center gap-[1.5rem] rounded-b-xl max-w-[90%]">
+                    <h2 className="xs:mt-[1rem]">Parties & Events</h2>
+                    <p className="max-w-[350px]">
+                      Book your party with us and enjoy full, private access to
+                      our facility. The venue will be exclusively yours, closed
+                      to the public. Regular snack & drink menu will be
+                      available *
+                    </p>
+                    <ButtonLink
+                      buttonText="Start Planning Your Event"
+                      buttonLink="/parties"
+                      buttonClass="hidden sm:block bg-soft-pink hover:bg-worm-pink"
+                    />
+                    <ButtonLink
+                      buttonText="Start Planning"
+                      buttonLink="/parties"
+                      buttonClass="sm:hidden bg-soft-pink hover:bg-worm-pink"
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
