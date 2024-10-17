4119dc8b436f3c0a40793e65a090263027924995
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-10-17 11:46:08 +0200

    add conditional banner to website

--- src/components/common/GlobalLayout.tsx
+++ src/components/common/GlobalLayout.tsx
@@ -9,11 +9,12 @@ export default function GlobalLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const bannerText = "We're not open yet.";
   return (
     <>
       <div className={quicksand.className}>
         <div className={`flex flex-col justify-between min-h-screen bg-white`}>
-          <Navbar />
+          <Navbar bannerText={bannerText} />
           <main className="block min-h-[100vh - 291px] w-full flex items-center justify-center">
             <div className="flex flex-col justify-center w-full max-w-[1200px] text-black">
               {children}
