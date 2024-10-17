6b5d326c6977324bb2f04a7a98d891db0112df0c
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-10-17 10:42:33 +0200

    update copy

--- src/components/about/AboutPage.tsx
+++ src/components/about/AboutPage.tsx
@@ -17,12 +17,9 @@ export default function AboutPage() {
         imageSide="right"
         imageClass="w-full max-w-[350px]"
         title="Sweeten Your Day with Saur Flour Cookies"
-        paragraph="Indulge in freshly baked delights from our owner’s passion project,
-      The Saur Flour. These custom royal iced sugar cookies are crafted with love and the
-      finest ingredients. Perfect for any occasion, they’re available for
-      special order—just reach out to us directly to place your order."
-        buttonText="Order Cookies"
-        buttonLink="mailto:thesaurflourbakery@yahoo.com"
+        paragraph="Treat your guests to freshly baked delights from our owner’s passion project, The Saur Flour. These custom royal iced sugar cookies, crafted with love and the finest ingredients, are available as an add-on option when you book your party with us. Let us know if you’d like to include them in your celebration!"
+        buttonText="Book a Party"
+        buttonLink="/parties"
         buttonClass="bg-soft-pink/50 hover:bg-soft-pink"
         allPadding={true}
         additionalButtonText="Cookie Gallery"
