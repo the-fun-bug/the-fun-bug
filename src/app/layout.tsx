9f6f59ea1e5b6f976acf09a06ffa081674f52e5c
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-10-20 13:33:54 +0200

    remove favicon from app folder, add multiple version of favicon and link to layout page with default metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,51 @@
 import React from 'react';
 import './globals.css';
 
+export const metadata = {
+  title: 'The Fun Bug, Play Studio & Parties',
+  description:
+    'Welcome to The Fun Bug, where playtime meets relaxation for both kids and parents!',
+  icons: {
+    icon: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        url: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        url: '/favicon-16x16.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        url: '/apple-touch-icon.png',
+      },
+      { rel: 'icon', type: 'image/x-icon', url: '/favicon.ico' },
+    ],
+  },
+  manifest: '/site.webmanifest',
+  iconsForAndroid: {
+    androidChrome: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192',
+        url: '/android-chrome-192x192.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '512x512',
+        url: '/android-chrome-512x512.png',
+      },
+    ],
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
