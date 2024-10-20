cd8f5c602b651b01295a0b4af6b27bc286215c36
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-10-20 13:37:41 +0200

    remove max width from images

--- src/components/common/ImageNextToText.tsx
+++ src/components/common/ImageNextToText.tsx
@@ -45,7 +45,7 @@ export default function ImageNextToTextSection({
         <Image
           src={imageSrc}
           alt={alt}
-          className={`${imageClass} md:max-w-[750px] lg:max-w-[45%]`}
+          className={`lg:w-[45%] ${imageClass}`}
         />
         <div className="flex flex-col gap-[1.25rem] max-w-[750px] lg:max-w-[490px] text-center lg:text-left items-center lg:items-start">
           <h1 className="font-nickainley">{title}</h1>
