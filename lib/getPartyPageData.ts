8e8b261e3268bbe2262dffe1b2c40c6af3a1466c
Leigh Dahlin <leigh.dahlin@gmail.com>
2025-02-07 10:11:27 +0100

    update party data function

--- lib/getPartyPageData.ts
+++ lib/getPartyPageData.ts
@@ -1,10 +1,10 @@
 import path from 'path';
 import fs from 'fs';
 import yaml from 'js-yaml';
-import { PartyPricing } from '@/types/types';
+import { PartyPageData } from '@/types/types';
 
 // Helper function to load the events page data
-export default function getPartyPricing(): PartyPricing {
+export default function getPartyPageData(): PartyPageData {
   const filePath = path.join(
     process.cwd(),
     'content/website-info/party-pricing.yml'
@@ -12,7 +12,7 @@ export default function getPartyPricing(): PartyPricing {
   const fileContents = fs.readFileSync(filePath, 'utf8');
 
   // Use js-yaml to parse the YAML file
-  const data = yaml.load(fileContents) as PartyPricing;
+  const data = yaml.load(fileContents) as PartyPageData;
 
   return data;
 }
