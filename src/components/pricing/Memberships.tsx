b89b0b6de382b2ae9aed947fa90def35e269730b
Leigh Dahlin <leigh.dahlin@gmail.com>
2025-04-30 10:04:24 +0200

    update wording and layout on pricing page

--- src/components/pricing/Memberships.tsx
+++ src/components/pricing/Memberships.tsx
@@ -12,31 +12,31 @@ export default function Memberships({
   membershipPricing: MembershipPricing;
 }) {
   const bullets = [
-    'There are no fees and no contract',
-    'Memberships are active for the month of purchase',
-    'Payments for memberships can be purchased in-store and drafted on the 1st of each month',
-    'Memberships include 1 reservation per day for each child',
+    'No fees, no contracts—cancel anytime',
+    'Memberships are valid for the month of purchase',
+    'Enjoy one play session per day, per child with your membership',
+    'After purchasing a membership, you’ll receive a coupon code to use when reserving play sessions — so you won’t pay at checkout every time',
   ];
   return (
     <section className={`flex items-center justify-center py-[3rem] px-[1rem]`}>
       <div className={`flex flex-col gap-[1.25rem] w-full max-w-[1200px]`}>
         <h2 className="font-nickainley pb-[1rem] text-center">Memberships</h2>
-        <div className="flex flex-col md:flex-row gap-[1.25rem] justify-between w-full">
-          <div className="flex flex-col gap-[1.25rem] md:w-[50%] md:items-center md:justify-center">
+        <div className="flex flex-col lg:flex-row gap-[1.25rem] justify-between w-full">
+          <div className="flex flex-col gap-[1.25rem] lg:w-[50%] items-center lg:justify-center">
             <div className="flex flex-col gap-[0.5rem]">
-              <div className="flex justify-between items-center mx-auto md:mx-0 gap-[2.5rem]">
+              <div className="flex justify-between items-center mx-auto lg:mx-0 gap-[2.5rem]">
                 <p className="text-[1rem] lg:text-[1.5rem]">1 Child</p>
                 <p className="text-[1rem] lg:text-[1.5rem]">
                   ${membershipPricing.one}/month
                 </p>
               </div>
-              <div className="flex justify-between items-center mx-auto md:mx-0 gap-[2.5rem]">
+              <div className="flex justify-between items-center mx-auto lg:mx-0 gap-[2.5rem]">
                 <p className="text-[1rem] lg:text-[1.5rem]">2 Children</p>
                 <p className="text-[1rem] lg:text-[1.5rem]">
                   ${membershipPricing.two}/month
                 </p>
               </div>
-              <div className="flex justify-between items-center mx-auto md:mx-0 gap-[2.5rem]">
+              <div className="flex justify-between items-center mx-auto lg:mx-0 gap-[2.5rem]">
                 <p className="text-[1rem] lg:text-[1.5rem]">3+ Children</p>
                 <p className="text-[1rem] lg:text-[1.5rem]">
                   ${membershipPricing.three}/month
@@ -51,9 +51,9 @@ export default function Memberships({
               excludeMargin={true}
             />
           </div>
-          <div className="grid grid-cols-1">
+          <ul className="grid grid-cols-1 gap-[1rem]">
             {bullets.map((r, i) => (
-              <div key={i} className="flex gap-[0.5rem] items-center">
+              <li key={i} className="flex gap-[0.75rem] items-center">
                 <Image
                   src={bulletImg}
                   alt="lady bug bullet"
@@ -62,9 +62,9 @@ export default function Memberships({
                   className="self-start"
                 />
                 <p className="lg:max-w-[460px]">{r}</p>
-              </div>
+              </li>
             ))}
-          </div>
+          </ul>
         </div>
       </div>
     </section>
