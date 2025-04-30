2df8d4346dfa70a01dfb6d5da85d336241f32fa4
Leigh Dahlin <leigh.dahlin@gmail.com>
2025-04-30 10:13:00 +0200

    fix typos on about page

--- src/components/about/HiEveryone.tsx
+++ src/components/about/HiEveryone.tsx
@@ -33,32 +33,33 @@ export default function HiEveryone({ photo }: { photo: string }) {
         <div className="py-[2rem] flex flex-col gap-[1.25rem] text-center">
           <h2 className="font-nickainley pb-[1rem]">Hi everyone!</h2>
           <p>
-            We are the Saur&apos;s. I am Kameron and my husband is Clay, or as
-            my sweet in-laws like to call it, the &lsquo;vision&lsquo; and the
-            &lsquo;labor&lsquo;. We have two amazing kiddos, Olivia and Levi. We
-            are big on nicknames around here! Our daughter Olivia is our Bug!
-            And our son Levi is our worm! Thus, The Fun Bug was born.
+            We are the Saurs. I am Kameron and my husband is Clay, or as my
+            sweet in-laws like to call it, the ‘vision’ and the ‘labor’. We have
+            two amazing kiddos, Olivia and Levi. We are big on nicknames around
+            here! Our daughter Olivia is our Bug! And our son Levi is our Worm!
+            Thus, The Fun Bug was born.
           </p>
+
           <p>
             As a mama of two small wigglers, I noticed a lack in the amount of
             indoor play opportunities here in New Braunfels, and decided to take
-            matters into my own hands. We want the Fun Bug to be a fun, clean,
+            matters into my own hands. We want The Fun Bug to be a fun, clean,
             safe, and aesthetic place for both kiddos and parents to enjoy.
-            Whether that is playing on our large custom built wooden play
+            Whether that is playing on our large custom-built wooden play
             structure, climbing on our soft play mats, or exploring creativity
             through our endless play opportunities, we know there is something
             for everyone.
           </p>
           <p>
             During open hours, we will have delicious drinks, comfortable
-            lounging, free wifi, and plenty of work space. Oh, and did I mention
+            lounging, free Wi-Fi, and plenty of workspace. Oh, and did I mention
             we are a party rental space after hours?! Host your event with full
-            access to our entire facility, reserved just for your party—closed
+            access to our entire facility, reserved just for your party — closed
             to the public for the ultimate private experience.
           </p>
           <p>
             We are so very excited for all that is to come and cannot wait to
-            meet each and everyone of you!
+            meet each and every one of you!
           </p>
           <div className="flex flex-col xs:flex-row items-center flex-wrap justify-center xs:gap-[2rem]">
             <button
