5d62da0c74d989e5e2a8c82b9d4bef4627890078
Leigh Dahlin <leigh.dahlin@gmail.com>
2026-01-27 15:53:29 +0100

    update rules of play

--- src/components/about/RulesOfPlay.tsx
+++ src/components/about/RulesOfPlay.tsx
@@ -4,14 +4,14 @@ import bulletImg from './img/bullet.png';
 
 export default function RulesOfPlay() {
   const rules = [
+    'To better support a safe and positive environment, open play will now be limited to children ages 6 and under. We’ve found that this adjustment allows our younger kiddos to play more freely and safely, which is at the heart of what The Fun Bug was created for.',
+    'For private parties only, children up to age 10 are still welcome to enjoy the play structure, as long as they are being respectful and mindful of younger children. We understand that many parties include older siblings and guests, and we’re happy to accommodate them in this setting.',
+    'No shoes are permitted on the grassy turf. Children and adults are required to wear socks. If you forget your child’s socks, you may purchase a pair at our counter for $3.',
+    'A Fun Bug staff member will announce when there are 10 minutes left in your play session. Please use this time to gather your belongings and prepare to exit so we can welcome the next group on time.',
     'Parents and guardians — not The Fun Bug staff — are responsible for their children and must remain on site at ALL times.',
     'The Fun Bug Play Studio & Parties is not responsible for lost or stolen items.',
-    'No shoes are permitted on the grassy turf. Children and adults are required to wear socks. If you forget your child’s socks, you may purchase a pair at our counter for $3.',
-    'Play equipment is designed for children 6 and under; use by older children or adults is not permitted.',
     'Please sanitize hands before entering the play area.',
     'Do not tape or pin anything to the walls.',
-    'All children aged 6 and under are welcome! Children six and under are sure to enjoy everything we offer. School-aged children are welcome but must use all equipment and toys safely, as intended, and be respectful of younger children playing nearby.',
-    'A Fun Bug staff member will announce when there are 10 minutes left in your play session. Please use this time to gather your belongings and prepare to exit so we can welcome the next group on time.',
     'No food or drinks are allowed in the play area.',
   ];
 
